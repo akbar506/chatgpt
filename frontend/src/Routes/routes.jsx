41761e955fbd363cc8b4c8a51fb9595cbc1c017c
Akbar Ali <m.akberali507@gmail.com>
2026-07-21 01:26:43 +0500

    Add Search page component and integrate search functionality for conversations

--- frontend/src/Routes/routes.jsx
+++ frontend/src/Routes/routes.jsx
@@ -4,6 +4,7 @@ const Home = lazy(() => import("@/pages/Home"));
 const Login = lazy(() => import("@/pages/Login"));
 const Register = lazy(() => import("@/pages/Register"));
 const Chat = lazy(() => import("@/pages/Chat"));
+const Search = lazy(() => import("@/pages/Search"))
 const AppLayout = lazy(() => import("@/layouts/AppLayout"));
 const AuthLayout = lazy(() => import("@/layouts/AuthLayout"));
 import { Routes, Route } from "react-router-dom";
@@ -21,7 +22,7 @@ export default function AppRoutes() {
             {/* Dashboard Pages */}
             <Route element={<AppLayout />}>
                 <Route path="/" element={<Home />} />
-                {/* <Route path="/search" element={<Search />} /> */}
+                <Route path="/search" element={<Search />} />
                 <Route path="/chat/:id" element={<Chat />} />
             </Route>
 
---
+++ frontend/src/pages/Search.jsx
@@ -0,0 +1,45 @@
+import { Input } from "@/components/ui/input"
+import { Search as SearchIcon } from "lucide-react"
+import { useState, useEffect } from "react"
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { Loader } from "lucide-react"
+
+export default function Search() {
+    const [searchQuery, setSearchQuery] = useState("");
+    const conversations = useSelector((state) => state.chat.conversations)
+    
+    return conversations.length > 0 ?(
+        <>
+            <div className="flex flex-col items-center h-full mt-20">
+                <div className="mb-32 w-full max-w-xl space-y-6">
+                    <div className="flex items-center rounded-4xl justify-between border-2 px-3 py-2 relative dark:bg-[#212121]">
+                        <SearchIcon />
+                        <Input
+                            placeholder="Search by title"
+                            className="border-0 focus-visible:ring-0 dark:bg-[#212121] field-sizing-content max-h-40 overflow-y-auto rounded-4xl min-h-8 font-normal resize-none outline-none text-lg sm:text-xl"
+                            value={searchQuery}
+                            onChange={(e) => setSearchQuery(e.target.value)}
+                            id="form-rhf-demo-title"
+                            autoComplete="off"
+                        />
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        <p className="text-lg font-semibold ml-2 text-muted-foreground">Recent</p>
+                        {conversations.filter((conversation) =>
+                            conversation.title.toLowerCase().includes(searchQuery.toLowerCase())
+                        ).map((conversation) => (
+                            <Link to={`/chat/${conversation._id}`} key={conversation._id} className="bg-[#212121] p-4 rounded-4xl">
+                                <div>
+                                    <h3 className=" ">{conversation.title}</h3>
+                                    <p className="text-gray-400">{conversation.lastMessage}</p>
+                                </div>
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+            </div>
+
+        </>
+    ): <><Loader className="animate-spin" /></>
+}
